ee975f06da7c7877f67a2cc8a1e1f2245ce8bc64
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-03-16 10:47:39 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from "express";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/me", function (req, res) {
+  res.json({
+    name: "Priyanjal Saxena",
+    email: "priyanjalsaxena7@gmail.com",
+    age: 21,
+    gender: "Male",
+  });
+});
+
+const port = process.env.PORT || 4000
+app.listen(port,function() {
+    console.log(`Server is running on port ${port}`)
+})
